6563a5dfd6e8d34dd3161cd2de57385e34487d28
Vic-8 <114266188+Vic-8@users.noreply.github.com>
2022-09-25 02:37:36 -0600

    Tarea1
    Tarea1

---
+++ Clase1/src/main/java/com/mycompany/clase1/Clase1.java
@@ -0,0 +1,133 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+package com.mycompany.clase1;
+
+import javax.swing.JOptionPane;
+import java.util.*;
+
+public class Clase1 {
+
+    public static void main(String[] args) {
+
+        /**
+         *
+         * @author victo
+         *
+         * public class Clase1 {
+         *
+         * public static void main(String[] args) { System.out.println("Hello
+         * World!"); JOptionPane.showMessageDialog(null,"Hello World");
+         *
+         * }
+         * }
+         * //
+         *
+         * /*
+         * /*
+         *
+         */
+        /*
+public class Clase1 {
+    
+    public static void main(String[] args) {
+
+int edad = Integer.parseInt(JOptionPane.showInputDialog("Ingrese Edad"));
+if (edad>=18){
+            JOptionPane.showMessageDialog(null,"Puede Votar");
+}
+else {
+            JOptionPane.showMessageDialog(null,"No Puede Votar");
+}
+}}
+/*
+         */
+        //Practica if
+        /*
+  int numeroDia = Integer.parseInt(JOptionPane.showInputDialog ("Digite el # de Dia"));
+  if (numeroDia==1) {
+      System.out.println("Lunes es dia laboral");
+  }
+  else if (numeroDia==2) {
+      System.out.println("Martes es dia laboral");
+  }
+  else if (numeroDia==3) {
+      System.out.println("Miercoles es dia laboral");
+  }
+  else if (numeroDia==4) {
+      System.out.println("Jueves es dia laboral");
+  }
+  else if (numeroDia==5) {
+      System.out.println("Viernes es dia laboral");
+  }
+  else if (numeroDia==6) {
+      System.out.println("Sabado es dia libre");
+  }
+  else if (numeroDia==7) {
+      System.out.println("Domingo es dia libre");
+  }
+    else {
+      System.out.println("Ingrese numero valido");
+  }
+      
+         */
+        //Ejercicio Double
+        /*
+ double numero1=Double.parseDouble(JOptionPane.showInputDialog("Digite Numero 1"));
+ double numero2=Double.parseDouble(JOptionPane.showInputDialog("Digite Numero 2"));
+ double numero3=Double.parseDouble(JOptionPane.showInputDialog("Digite Numero 3"));
+ double numero4=Double.parseDouble(JOptionPane.showInputDialog("Digite Numero 4"));
+
+ if (numero1>numero2 && numero1>numero3 && numero1>numero4)
+     JOptionPane.showMessageDialog(null,"Numero mayor es " +numero1);
+ 
+ else if (numero2>numero1 && numero2>numero3 && numero2>numero4)
+     JOptionPane.showMessageDialog(null,"Numero mayor es " +numero2);
+ else if (numero3>numero1 && numero3>numero2 && numero3>numero4)
+     JOptionPane.showMessageDialog(null,"Numero mayor es " +numero3);
+ 
+ else {
+ JOptionPane.showMessageDialog(null,"Numero mayoor es " +numero4);
+ }
+ 
+         */
+ /*
+  //UpperCase
+  
+  String nombre= JOptionPane.showInputDialog("Digite su nombre").toUpperCase();
+        System.out.println("Nombre es "+nombre);
+  
+         */
+        String lectura;
+        int numero;
+
+        lectura = JOptionPane.showInputDialog("Digite un numero a evaluar ");
+        System.out.println("Ud ha digitado " + lectura);
+
+        try {
+            numero = Integer.parseInt(lectura);
+
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "Debe de digitar un numero");
+        }
+        // Por qué debo de definir otra vez numero como un integer si ya lo hice arriba?
+        numero = Integer.parseInt(lectura);
+        if (numero % 2 == 0) {
+            if (numero > 0) {
+                JOptionPane.showMessageDialog(null, "El siguiente numero es par y positivo: " + numero);
+            }
+            if (numero < 0) {
+                JOptionPane.showMessageDialog(null, "El siguiente numero es par y negativo: " + numero);
+            }
+        } else if (numero % 2 != 0) {
+            if (numero > 0) {
+                JOptionPane.showMessageDialog(null, "El siguiente numero es Impar y positivo: " + numero);
+            }
+            if (numero < 0) {
+                JOptionPane.showMessageDialog(null, "El siguiente numero es Impar y negativo: " + numero);
+            }
+        }
+    }
+
+}
